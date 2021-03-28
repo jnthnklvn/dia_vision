f8f1c21483d73dd543e404044d9d72b81da76470
Jonathan Kelvin <j_kelvin1997@hotmail.com>
2021-03-27 22:56:58 -0300

    :bug: codificação não é reconhecida pelo Windows

--- lib/app/shared/utils/file_utils.dart
+++ lib/app/shared/utils/file_utils.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
 import 'dart:io';
 
 class FileUtils {
   Future<File> save(String content, String path) async {
     File file = File(path);
-    return await file.writeAsString(content);
+    List<int> bytes = [0xEF, 0xBB, 0xBF] + utf8.encode(content);
+    return await file.writeAsBytes(bytes);
   }
 }
