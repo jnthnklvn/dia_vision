8bee1e19fe89c2f2527d1a353cbbdeef5c4dd644
Jonathan Kelvin <j_kelvin1997@hotmail.com>
2021-05-09 22:00:13 -0300

    :sparkles: assistente de voz para erros e confirmações

--- lib/app/shared/utils/scaffold_utils.dart
+++ lib/app/shared/utils/scaffold_utils.dart
@@ -1,11 +1,20 @@
 import 'package:dia_vision/app/shared/components/snack_bars.dart';
 
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:flutter/material.dart';
 
 class ScaffoldUtils {
+  Future _speak(String txt) => Modular.get<FlutterTts>().speak(txt);
+
   final scaffoldKey = GlobalKey<ScaffoldState>();
-  void onError(String message) =>
-      scaffoldKey.currentState?.showSnackBar(SnackBars.error(message));
-  void onSuccess(String message) =>
-      scaffoldKey.currentState?.showSnackBar(SnackBars.confirmation(message));
+  void onError(String message) {
+    scaffoldKey.currentState?.showSnackBar(SnackBars.error(message));
+    _speak(message);
+  }
+
+  void onSuccess(String message) {
+    scaffoldKey.currentState?.showSnackBar(SnackBars.confirmation(message));
+    _speak(message);
+  }
 }
