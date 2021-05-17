746e9ff142a869f6aa45e0e3723aed320ff41597
Jonathan Kelvin <jnthn.klvn.s@gmail.com>
2021-05-17 18:12:28 -0300

    :sparkles: ajustes e configuração projeto ios

---
+++ ios/AwesomeContentExtension/NotificationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationViewController.swift
+//  AwesomeContentExtension
+//
+//  Created by Jonathan Kelvin de Jesus Santos 2 on 17/05/21.
+//
+
+import awesome_notifications
+
+@available(iOS 10.0, *)
+class NotificationViewController: AwesomeContentExtension {
+
+}
