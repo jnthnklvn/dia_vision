f481e5cc0c25dbdb300980b0729c417fa5eab0a4
Jonathan Kelvin <jnthn.klvn.s@gmail.com>
2022-04-18 22:54:30 -0300

    :sparkles: organizando chaves

---
+++ lib/app/shared/utils/strings.dart
@@ -0,0 +1,126 @@
+/// Descriptions
+const medicationRegister = "Registrar medicação";
+const feetCheckRegister = "Avaliar pés";
+const suggestMedicalCenterRegister = "Sugerir Centro de Saúde";
+const suggestVisionAppRegister = "Sugerir App de Visão";
+const exerciseRegister = "Registrar atividade física";
+const alimentationRegister = "Registrar alimentação";
+const searchFood = "Pesquisar alimento";
+const addFood = "Adicionar alimento";
+const delFood = "Remover alimento";
+const addTime = "Adicione um horário";
+const allowNotifications = "Permitir notificações";
+const diuresisCheckRegister = "Avaliar diurese";
+const glicemyRegister = "Registar glicemia";
+const withoutMedicationsRegistered = "Sem medicações cadastradas";
+const withoutFeetCheckRegistered = "Sem avaliações cadastradas";
+const withoutMedicalCenterRegistered = "Sem centros registrados";
+const withoutVisionAppRegistered = "Sem apps registrados";
+const noFoodsFound = "Não foram encontrados alimentos";
+const withoutExercisesRegistered = "Sem registros de exercício";
+const withoutAlimentationRegistered = "Sem registros de alimentação";
+const withoutDiuresisRegistered = "Sem registros de diurese";
+const withoutSelfCareRegistered = "Sem registros de autocuidado";
+const withoutGlicemyRegistered = "Sem registros de glicemia";
+const forgotPasswordQst = "Esqueceu sua senha? ";
+const recoveryPassword = "Recuperar senha.";
+const doesntHaveAccountQst = "Não possui uma conta? ";
+const hasAccountQst = "Já possui uma conta? ";
+const clickToSearchFood = "Clique para pesquisar alimento";
+const notEnoughLetters = "A busca deve haver pelos menos 3 letras";
+const wishToRemoveFood = "Deseja remover esse alimento da sua refeição?";
+const wishToAllowNotifications = "Permite que sejam enviadas notificações?";
+const selectType = "Selecione o tipo";
+const registeredWithSuccess = "Registro realizado com sucesso!";
+const notificationSuccessEnabled = "Notificação ativada com sucesso";
+const notificationSuccessDisabled = "Notificação desativada com sucesso";
+const clearSearchText = "Limpar texto da pesquisa";
+const closeSearchPage = "Fechar página de pesquisa";
+const glicemyRegisterTime = "Hora de regristrar sua glicemia";
+const toGlicemyRegisterTime = "para o horário de glicemia";
+const introPage1 =
+    'Ao utilizar este aplicativo, você pode manter os componentes '
+    'como textos e botões pressionados para ouvir suas descrições... ';
+const introPage2 =
+    'Há um botão flutuante posicionado na parte inferior da tela. Ao clicar '
+    'nesse botão, um menu é aberto. Nele você pode personalizar a tela do '
+    'aplicativo e ativar ou desativar o retorno por voz. Também é possível '
+    'ocultar o botão de acessibilidade nesse menu.';
+const allCategories = "Todas as categorias";
+const allTypes = "Todas os tipos";
+const unknowError = "Erro desconhecido";
+const turnOnDarkMode = 'Ativar modo escuro';
+const turnOffDarkMode = 'Desativar modo escuro';
+const turnOnVoiceFeedback = 'Ativar retorno por voz.';
+const turnOffVoiceFeedback = 'Desativar retorno por voz.';
+const customFontSizeDesc = 'Toque nos ícones de mais ou menos '
+    'para aumentar ou diminuir o tamanho das letras.';
+const fontSize = 'Tamanho da fonte';
+const incFontSize = 'Botão: aumentar tamanho da fonte.';
+const decFontSize = 'Botão: diminuir tamanho da fonte.';
+const accButtonDesc = 'Botão de acessibilidade:'
+    ' toque para acessar o menu de opções.';
+const goToSettings = 'Ir para configurações';
+const hideAccessibilityButton = 'Ocultar botão de acessibilidade';
+const showAccessibilityButton = 'Mostrar botão de acessibilidade';
+const goToIntro = 'Ir para tela de introdução';
+const goToBegin = 'Voltar à tela inicial';
+const goToBeginDesc = 'Toque para ser redirecionado para tela inicial.';
+const goToSettingsDesc = 'Toque para ser redirecionado para '
+    'tela de configurações.';
+const goToIntroDesc = 'Toque para ser redirecionado para a tela de'
+    'introdução com as instruções para utilização do aplicativo.';
+const logoSemanticDesc = "Logomarca do aplicativo. A imagem apresenta o nome "
+    "do aplicativo com um olho entre as palavras Dia e Vision.";
+const walkingManSemanticDesc =
+    "A ilustração mostra um homem com deficiência visual com um óculos escuro "
+    "e uma bengala atravessando uma faixa de pedestre .";
+const introWomanSemanticDesc =
+    "A ilustração mostra uma mulher em uma cadeira de rodas com um celular "
+    "na mão esquerda e a representação de um bloco de conteúdos da internet "
+    "erguida na mão direita.";
+const profileImgSemanticDesc = "Imagem de perfil. Desabilitado no momento.";
+
+// Erros
+const registerNotificationFail = 'Erro ao tentar ativar notificação. Cheque se '
+    'as informações sobre horário e posologia foram informadas.';
+const deleteNotificationFail = 'Erro ao tentar desativar notificação.'
+    ' Tente novamente mais tarde.';
+
+/// Words
+const backStr = "voltar";
+const cancellStr = "Cancelar";
+const notificationStr = "notificação";
+const enableStr = "ativar";
+const disableStr = "desativar";
+const passwordStr = "Senha";
+const medicationStr = "Medicações";
+const feetCheck = "Avaliações dos pés";
+const medicalCenters = "Centros de Saúde";
+const visionApps = "Apps de Visão";
+const exercisesStr = "Atividade Fisíca";
+const alimentationStr = "Alimentação";
+const kidneyDiuresis = "Rins e diurese";
+const selfCareTitle = "Autocuidado";
+const medicalCenterTitle = "Centro de Saúde";
+const visionAppTitle = "App de Visão";
+const glicemyStr = "Glicemia";
+const signUp = "Registre-se.";
+const registryStr = "Registro";
+const signIn = "Entre.";
+const shareStr = "Compartilhar";
+const changeStr = "Alterar";
+const categoryStr = "Categoria";
+const typeStr = "Tipo";
+const addStr = "Adicionar";
+const suggestStr = "Sugerir";
+const searchStr = "Pesquisar";
+const moduleStr = "Módulo";
+const settingsStr = "Configurações";
+
+/// Buttons
+const buttonStr = "Botão";
+
+/// Rotas
+const registerStr = "registrar";
+const selfCareArticleRoute = "selfCareArticle";
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:dia_vision/app/shared/utils/constants.dart';
-import 'package:dia_vision/app/shared/utils/strings.dart';
 import 'package:dia_vision/app/app_module.dart';
 import 'package:dia_vision/app/app_widget.dart';
 
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:parse_server_sdk/parse_server_sdk.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
 
@@ -36,15 +36,16 @@ Future<void> initializateNotifications() async {
 
 Future<void> initializeParser() async {
   final appDocDir = await getApplicationDocumentsDirectory();
+  await dotenv.load(fileName: ".env");
 
   await Parse().initialize(
-    kParseApplicationId,
-    kParseBaseUrl,
-    clientKey: kParseClientKey,
+    dotenv.env['kParseApplicationId']!,
+    dotenv.env['kParseBaseUrl']!,
+    clientKey: dotenv.env['kParseClientKey'],
     autoSendSessionId: true,
     coreStore: await CoreStoreSembastImp.getInstance(
       appDocDir.path + "/data",
-      password: kParseApplicationId,
+      password: dotenv.env['kParseApplicationId'],
     ),
   );
 }
